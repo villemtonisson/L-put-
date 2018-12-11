d29f4dbc990f0c412834f59f08663129bf5ee239
Villem Tõnisson <villemtonisson@gmail.com>
2018-12-11 23:51:05 +0200

    Zip failide avamine olemas

---
+++ Lahtipakkimine.py
@@ -0,0 +1,17 @@
+#https://www.geeksforgeeks.org/working-zip-files-python/
+# importing required modules 
+from zipfile import ZipFile 
+  
+# specifying the zip file name 
+file_name = "pakk.zip"
+  
+# opening the zip file in READ mode 
+with ZipFile(file_name, 'r') as zip: 
+    # printing all the contents of the zip file 
+  #  zip.printdir() 
+    print(zip.namelist())
+    zip.extract('2016-10-02_13-31-39_0.txt')
+    # extracting all the files 
+    #print('Extracting all the files now...') 
+    #zip.extractall() 
+    #print('Done!') 
