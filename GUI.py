02eba063fbf27f40d4b1255a10697297d5ae2f2b
villemtonisson <villemtonisson@gmail.com>
2019-03-16 16:37:46 +0200

    All modes should now be working

--- GUI.py
+++ GUI.py
@@ -1,10 +1,13 @@
 import tkinter as tk
 import tkinter.ttk as ttk
-from tkinter.filedialog import askopenfilename, askdirectory
+import json
+from tkinter.filedialog import askopenfilename, askopenfilenames
 from pathlib import Path
 from datepicker import Datepicker
-from unpacker import unpack_between_filenames, unpack_between_infile
+from splitter import separate_by_ids
+from unpacker import unpack_between_infile, unpack_all
 from datetime import datetime
+
 
 if __name__ == "__main__":
     loc_7z = Path("C:\\Program Files\\7-Zip\\7z.exe")
@@ -22,46 +25,113 @@ if __name__ == "__main__":
 #start=datetime(2016, 11, 26, hour=12, minute=20)
 #end=datetime(2017, 9, 9, hour=16, minute=0)
 
-
+"""
+Olenevalt kasutaja sisestusest, valib kas mitu või ühe faili
+"""
 def pick_file():
-    global chosen_file
-    filename=Path(askopenfilename(title="Select file", filetypes=(("all files", "*.*"),
+    global chosen_file, chosen_mode_var
+    
+    mode=chosen_mode_var.get()
+    #1 file
+    if mode=="1log":
+        filename=Path(askopenfilename(title="Select file", filetypes=(("all files", "*.*"), ("text files", "*.txt"))))
+        print(filename)
+        chosen_file_var.set(filename)
+    #1 archive
+    if mode=="1arch":
+        filename=Path(askopenfilename(title="Select file", filetypes=(("all files", "*.*"),
                 ("RAR files", "*.rar"), ("ZIP files", "*.zip"),("7z files", "*.7z"),("TAR files", "*.tar"))))
-    print(Path(filename))
-    chosen_file_var.set(filename)
+        print(filename)
+        chosen_file_var.set(filename)
+    if mode=="2log":
+        filenames = askopenfilenames(
+                filetypes = [('Text', '.txt'), ('all files', '.*'),],
+                title = "Select .txt files",
+                multiple = True,
+                )
+        chosen_file_var.set(json.dumps(list(filenames)))
+
+    if mode=="2arch":
+        filenames = askopenfilenames(
+                defaultextension = '*.*',
+                filetypes = [("all files", "*.*"), ("RAR files", "*.rar"),
+                             ("ZIP files", "*.zip"),("7z files", "*.7z"),("TAR files", "*.tar"),],
+                title = "Select archive files",
+                multiple = True,
+                )
+        chosen_file_var.set(json.dumps(list(filenames)))
+    
+    
     return
 
-
+"""
+Olenevalt kasutaja valikust, kas pakib lahti või ainult eraldab ülesannete kaupa
+"""
 def unpack():
     global start_date_picker, start_hour_picker, start_minute_picker
     global end_date_picker, end_hour_picker, end_minute_picker, chosen_file_field
+    global separate_exercises_var, use_dates_var, chosen_mode_var
     
-    start_date_str=start_date_picker.current_text+"-"+start_hour_picker.get()+"-"+start_minute_picker.get()
-    start_date=datetime.strptime(start_date_str, "%Y-%m-%d-%H-%M")
+    separate=(separate_exercises_var.get()==1)
+    use_dates=(use_dates_var.get()==1)
+    mode=chosen_mode_var.get()
+    #Siia tulevad failinimed, mis lahti pakiti
+    filenames=[]
     
-    end_date_str=end_date_picker.current_text+"-"+end_hour_picker.get()+"-"+end_minute_picker.get()
-    end_date=datetime.strptime(end_date_str, "%Y-%m-%d-%H-%M")
+    if use_dates:
+        start_date_str=start_date_picker.current_text+"-"+start_hour_picker.get()+"-"+start_minute_picker.get()
+        start_date=datetime.strptime(start_date_str, "%Y-%m-%d-%H-%M")
+        
+        end_date_str=end_date_picker.current_text+"-"+end_hour_picker.get()+"-"+end_minute_picker.get()
+        end_date=datetime.strptime(end_date_str, "%Y-%m-%d-%H-%M")
+        
+    #1 file
+    if mode=="1log":
+        filenames.append(chosen_file_field.get())
+    #1 archive
+    elif mode=="1arch":
+        if use_dates:
+            filenames=unpack_between_infile(loc_7z, chosen_file_field.get(), start_date, end_date)
+            print(filenames)
+        else:
+            filenames=unpack_all(loc_7z, chosen_file_field.get())
+            print(filenames)
+        
+    elif mode=="2log":
+        if use_dates:
+            pass
+        else:
+            filenames=json.loads(chosen_file_field.get())
+    elif mode=="2arch":
+        if use_dates:
+            for f in json.loads(chosen_file_field.get()):
+                filenames.extend(unpack_between_infile(loc_7z, f, start_date, end_date))
+        else:
+            for f in json.loads(chosen_file_field.get()):
+                filenames.extend(unpack_all(loc_7z, f))
     
-    print(start_date, end_date)
-    
-    unpack_between_infile(chosen_file_field.get(), start_date, end_date)
+    if separate:
+        for f in filenames:
+            separate_by_ids(f)
     
     return
+
+background='ghost white'
 
 window = tk.Tk()
 window.title("Log File Editor")
 window.geometry('800x600')
-window.configure(background='white')
+window.configure(background=background)
 window.resizable(0, 0)
 
 # Frames for the app
-titleFrame = tk.Frame(window, width=800, height=100, background='blue')
-middleFrame = tk.Frame(window, width=800, height=300, background='cyan')
-leftFrame = tk.Frame(middleFrame, width=400, height=300, background='red')
-rightFrame = tk.Frame(middleFrame, width=400, height=300, background='green')
-bottomFrame= tk.Frame(window, width=800, height=200, background='gray')
-leftBottomFrame= tk.Frame(bottomFrame, width=400, height=200, background='yellow')
-rightBottomFrame= tk.Frame(bottomFrame, width=400, height=200, background='cyan')
+titleFrame = tk.Frame(window, width=800, height=100, background=background)
+middleFrame = tk.Frame(window, width=800, height=300, background=background)
+leftFrame = tk.Frame(middleFrame, width=400, height=300, background=background)
+rightFrame = tk.Frame(middleFrame, width=400, height=300, background=background)
+bottomFrame= tk.Frame(window, width=800, height=200, background=background)
+leftBottomFrame= tk.Frame(bottomFrame, width=400, height=200, background=background)
+rightBottomFrame= tk.Frame(bottomFrame, width=400, height=200, background=background)
 
 
 #No resizing frames
@@ -84,23 +154,23 @@ rightBottomFrame.pack(anchor="n", side=tk.RIGHT)
 
 ## TOP
 #Title
-title_label = tk.Label(titleFrame, text="Log File Editor", bg="white", font=("Arial Bold", 30), pady=15)
+title_label = tk.Label(titleFrame, text="Log File Editor", bg=background, font=("Arial Bold", 30), pady=15)
 title_label.pack()
 
 ### MIDDLE
 ## LEFT
 #Elements on the left
-start_label = tk.Label(leftFrame, text="Start", bg="white", font=("Arial Bold", 14))
+start_label = tk.Label(leftFrame, text="Start", bg=background, font=("Arial Bold", 14))
 start_label.pack(anchor='c')
 
-start_date_label = tk.Label(leftFrame, text="Date", bg="white", font=("Arial", 12))
+start_date_label = tk.Label(leftFrame, text="Date", bg=background, font=("Arial", 12))
 start_date_label.pack(anchor='c')
 
 start_date_picker = Datepicker(leftFrame)
 start_date_picker.pack(anchor="c")
 
 #Hour
-start_hour_label = tk.Label(leftFrame, text="Hour", bg="white", font=("Arial", 12))
+start_hour_label = tk.Label(leftFrame, text="Hour", bg=background, font=("Arial", 12))
 start_hour_label.pack(anchor='c')
 
 start_hour_picker = ttk.Combobox(leftFrame)
@@ -109,7 +179,7 @@ start_hour_picker.current(0) #set the selected item
 start_hour_picker.pack(anchor='c')
 
 #Minute
-start_minute_label = tk.Label(leftFrame, text="Minute", bg="white", font=("Arial", 12))
+start_minute_label = tk.Label(leftFrame, text="Minute", bg=background, font=("Arial", 12))
 start_minute_label.pack(anchor='c')
 
 start_minute_picker = ttk.Combobox(leftFrame)
@@ -119,17 +189,17 @@ start_minute_picker.pack(anchor='c')
 
 ## RIGHT
 #Elements on the right
-end_label = tk.Label(rightFrame, text="End", bg="white", font=("Arial Bold", 12))
+end_label = tk.Label(rightFrame, text="End", bg=background, font=("Arial Bold", 12))
 end_label.pack(anchor='c')
 
-end_date_label = tk.Label(rightFrame, text="Date", bg="white", font=("Arial", 12))
+end_date_label = tk.Label(rightFrame, text="Date", bg=background, font=("Arial", 12))
 end_date_label.pack(anchor='c')
 
 end_date_picker = Datepicker(rightFrame)
 end_date_picker.pack(anchor="c")
 
 #Hour
-end_hour_label = tk.Label(rightFrame, text="Hour", bg="white", font=("Arial", 12))
+end_hour_label = tk.Label(rightFrame, text="Hour", bg=background, font=("Arial", 12))
 end_hour_label.pack(anchor='c')
 
 end_hour_picker = ttk.Combobox(rightFrame)
@@ -138,7 +208,7 @@ end_hour_picker.current(0) #set the selected item
 end_hour_picker.pack(anchor='c')
 
 #Minute
-end_minute_label = tk.Label(rightFrame, text="Minute", bg="white", font=("Arial", 12))
+end_minute_label = tk.Label(rightFrame, text="Minute", bg=background, font=("Arial", 12))
 end_minute_label.pack(anchor='c')
 
 end_minute_picker = ttk.Combobox(rightFrame)
@@ -162,7 +232,7 @@ unpack_button.pack(anchor='c')
 
 ## LEFTBOTTOM
 # Select mode label
-select_mode_label = tk.Label(leftBottomFrame, text="Select mode", bg="white", font=("Arial", 11))
+select_mode_label = tk.Label(leftBottomFrame, text="Select mode", bg=background, font=("Arial", 11))
 select_mode_label.pack(anchor='c')
 
 # Creating radiobuttons for selecting mode
--- unpacker.py
+++ unpacker.py
@@ -13,9 +13,6 @@ if __name__ == "__main__":
         except:
             loc_7z = Path("C:\\Program Files\\7-Zip\\7z.exe")
             
-    
-
-
 """
 Võtab antud käsu ja käivitab selle käsurealt
 Väljastab kõik read, mida käsu täitmisel näidataks
@@ -32,7 +29,7 @@ def run_process(command):
 """
 Võtab arhiivi nime ja tagastab kõikide seal olevate failide nimed
 """
-def get_filenames(arch_loc):
+def get_filenames(loc_7z, arch_loc):
     names=False
     name_list=[]
     prev_line=""
@@ -57,51 +54,38 @@ def get_filenames(arch_loc):
 Avab arhiivi ja võtab seal välja failid, mille nimed on antud
 Argumentideks on arhiivi asukoht, failinimed, väljundkaust
 """
-def unpack(arch_loc, filenames, output='*'):
+def unpack(loc_7z, arch_loc, filenames, output='*'):
     command = str(loc_7z)+" e "+ arch_loc +" -aoa -o"+output+" "+" ".join(filenames)
     for i in run_process(command):
         print(i, end="")
-    
-
-"""
-Avab arhiivi ja võtab sealt välja failid, mille nimi on õiges ajavahemikus
-Kui mõne logifaili nime on muudetud, võtab ka selle välja
-"""
-def unpack_between_filenames(arch_loc, start, end, output='*'):
-    all_filenames=get_filenames(arch_loc)
-    filenames=[]
-    for name in all_filenames:
-        #Juhuks, kui mõne faili nime on muudetud
-        try:
-            date=datetime.strptime(name.rsplit("_", 1)[0], '%Y-%m-%d_%H-%M-%S')
-            if date>start and date<end:
-                filenames.append(name)
-        except:
-            filenames.append(name)
-            #pass , juhuks, kui ei taha muudetud nimega
-    unpack(arch_loc, filenames, output=output)
- 
  
 """
 Avab arhiivi ja võtab sealt välja kõik failid
+Tagastab failinimed
 """
-def unpack_all(arch_loc, output='*'):
-    all_filenames=get_filenames(arch_loc)
-    filenames=[]
-    unpack(arch_loc, filenames, output=output)
+def unpack_all(loc_7z, arch_loc, output='*'):
+    all_filenames=get_filenames(loc_7z, arch_loc)
+    if output=='*':
+        output, ext = os.path.splitext(arch_loc)
+    
+    unpack(loc_7z, arch_loc, [], output=output)
+        
+    filenames=[str(Path(output).joinpath(x)) for x in all_filenames]
+    return filenames
     
 """
 Avab arhiivi ja võtab sealt välja kõik failid
 Seejärel vaatab failid läbi ja eemaldab need, mis ei kuulu antud ajavahemikku
 
 Tulemuskaust peab olema tühi selle töötamiseks
+Tagastab allesjäänud failide nimed
 """  
-def unpack_between_infile(arch_loc, start, end, output='*'):
-    all_filenames=get_filenames(arch_loc)
-    unpack_all(arch_loc, output=output)
+def unpack_between_infile(loc_7z, arch_loc, start, end, output='*'):
+    all_filenames=get_filenames(loc_7z, arch_loc)
     if output=='*':
-        output=arch_loc.rsplit(".", 1)[0]
-    for name in all_filenames:
+        output, ext = os.path.splitext(arch_loc)
+    unpack_all(loc_7z, arch_loc, output=output)
+    for name in all_filenames[:]:
         to_remove=True
         p=Path(output).joinpath(name)
         with open(p, 'r', encoding="UTF8") as f:
@@ -114,12 +98,33 @@ def unpack_between_infile(arch_loc, start, end, output='*'):
                 to_remove=True
         if to_remove:
             os.remove(p)
-                
-            
+            all_filenames.remove(name)
+    filenames=[str(Path(output).joinpath(x)) for x in all_filenames]
+    return filenames
+
+"""
+Avab arhiivi ja võtab sealt välja failid, mille nimi on õiges ajavahemikus
+Kui mõne logifaili nime on muudetud, võtab ka selle välja
+
+def unpack_between_filenames(loc_7z, arch_loc, start, end, output='*'):
+    all_filenames=get_filenames(loc_7z, arch_loc)
+    filenames=[]
+    for name in all_filenames:
+        #Juhuks, kui mõne faili nime on muudetud
+        try:
+            date=datetime.strptime(name.rsplit("_", 1)[0], '%Y-%m-%d_%H-%M-%S')
+            if date>start and date<end:
+                filenames.append(name)
+        except:
+            filenames.append(name)
+            #pass , juhuks, kui ei taha muudetud nimega
+    unpack(loc_7z, arch_loc, filenames, output=output)
+    return filenames
+"""
 #start=datetime(2016, 11, 26, hour=12, minute=20)
 #end=datetime(2017, 9, 9, hour=16, minute=0)
 
 #unpack_between_infile("C:/Loputoo/L-put-/pakk.7z", start, end, output='C:/Loputoo/L-put-/pakk2')
 
-#unpack(arch_loc, filenames, output="C:\\Users\\Villem\\Desktop\\L-put-\\pakk1")
+#unpack(loc_7z, arch_loc, filenames, output="C:\\Users\\Villem\\Desktop\\L-put-\\pakk1")
 #print(get_filenames("C:/Loputoo/L-put-/pakk.7z"))    
